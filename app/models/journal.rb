a8dc0436f85daff0cc835cbcfc51355a790448b2
Mark Jones <mark@mmadvertising.co.uk>
2016-12-14 15:44:54 +0000

    Added Model Validations to tweets and journals

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,3 +1,12 @@
 class Journal < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :content1, presence: true, length: { maximum: 140 }
+  validates :content2, presence: true, length: { maximum: 140 }
+  validates :content3, presence: true, length: { maximum: 140 }
+  validates :content4, presence: true, length: { maximum: 140 }
+  validates :content5, presence: true, length: { maximum: 140 }
+  validates :content6, presence: true, length: { maximum: 140 }
+  validates :content7, presence: true, length: { maximum: 560 }
 end
