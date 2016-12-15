c3394464f135a4d4ece39ef45684bdd3328fc8bb
Mark Jones <mark@mmadvertising.co.uk>
2016-12-15 16:19:26 +0000

    added images to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,10 @@ class Tweet < ApplicationRecord
 
   validates :user, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
+  # Explicitly do not validate
+  do_not_validate_attachment_file_type :image
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Paperclip.options[:command_path] = "/c/WINDOWS/system32/convert"
